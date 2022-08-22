084fef984fb7caabef4baebfeadac802fd4cc65a
muller <gram@lg.com>
2022-08-22 10:35:41 +0800

    gradle demo

---
+++ src/test/java/com/example/gradledemo/GradledemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.gradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradledemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test() {
+
+    }
+
+}

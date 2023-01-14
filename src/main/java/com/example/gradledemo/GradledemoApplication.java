1d7455744c0de39a876c1a6ddb784c7c0cc1e6d6
muller <mulleriloveyou@gmail.com>
2023-01-14 20:23:37 +0800

    test

--- src/main/java/com/example/gradledemo/GradledemoApplication.java
+++ src/main/java/com/example/gradledemo/GradledemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GradledemoApplication {
     public static void main(String[] args) {
+        System.out.println();
         SpringApplication.run(GradledemoApplication.class, args);
     }
 
